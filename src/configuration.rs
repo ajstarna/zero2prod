0361150188cc794aa45043d6f12614b21f2f403e
Adam St Arnaud <ajstarna@ualberta.ca>
2022-08-03 23:50:36 -0600

    using type driven development for validation on user name

--- src/configuration.rs
+++ src/configuration.rs
@@ -89,18 +89,18 @@ impl TryFrom<String> for Environment {
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
         let mut options = self.without_db().database(&self.database_name);
-	options.log_statements(tracing::log::LevelFilter::Trace);
-	options
+        options.log_statements(tracing::log::LevelFilter::Trace);
+        options
     }
 
     pub fn without_db(&self) -> PgConnectOptions {
-	let ssl_mode = if self.require_ssl {
-	    PgSslMode::Require
-	} else {
-	    // try and encrypted connection, fall back to unencrypted if it fails
-	    PgSslMode::Prefer
-	};
-	
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            // try and encrypted connection, fall back to unencrypted if it fails
+            PgSslMode::Prefer
+        };
+
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
